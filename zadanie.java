cf76f64dfa022aa67851479e0f5e4e68c07d0a0e
Safronov Evgeniy <safronov.evgenijon@mail.ru>
2021-09-21 20:01:04 +0300

    2-й разраб

--- zadanie.java
+++ zadanie.java
@@ -1,6 +1,6 @@
 public class zadanie {
 	public static void main (String[] args){
 		System.out.println("Привет");
-		System.out.println("Пока");
+		System.out.println("Привет от другого разработчика");
 	}
 }
